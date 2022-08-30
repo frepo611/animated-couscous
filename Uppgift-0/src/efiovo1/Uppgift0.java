0aa4c639ee6d850766fdc56b8f51cf078dcf709a
Fredrik von Post <fredrik.vonpost@maxar.com>
2022-08-30 08:19:08 +0200

    first commit

---
+++ Uppgift-0/src/efiovo1/Uppgift0.java
@@ -0,0 +1,13 @@
+package efiovo1;
+
+/**
+ * Skriver ut texten "Hello World!"
+ * @author Fredrik von Post, efiovo-1
+ */
+
+public class Uppgift0 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
