736fd9dad8619ed3f146a7eb3179fa9bf4a42314
Fredrik von Post <fredrik.vonpost@maxar.com>
2022-09-15 10:05:52 +0200

    class variable rate is common for all objects of the class

--- DefinieraEgnaKlasser/src/App.java
+++ DefinieraEgnaKlasser/src/App.java
@@ -6,18 +6,15 @@ public class App {
         Rectangle rectangle3 = new Rectangle(3.3, 4.5);
 
         SavingsAccount saving1 = new SavingsAccount("2000", "konto1");
-        saving1.calculateMonthlyInterest();
         System.out.println(saving1);
-
-        SavingsAccount saving2 = new SavingsAccount("2000", "konto2");
-        saving2.calculateMonthlyInterest();
+        SavingsAccount.changeRate("0.01");
+        SavingsAccount saving2 = new SavingsAccount("3000", "konto2");
         System.out.println(saving2);
 
-        saving1.changeRate("0.01");
-        saving2.changeRate("0.02");
+
 
         for (int i = 0; i < 12; i++) {
-            System.out.printf("Månad %1$d %s:%s%nMånad %1$d %s:%s", i, saving1.getName(), saving1, saving2.getName(), saving2 );
+            System.out.printf("Månad %1$d %2$s:%3$s %4$s:%5$s%n", i, saving1.getName(), saving1, saving2.getName(), saving2);
             saving1.calculateMonthlyInterest();
             saving2.calculateMonthlyInterest();
                     }
--- DefinieraEgnaKlasser/src/SavingsAccount.java
+++ DefinieraEgnaKlasser/src/SavingsAccount.java
@@ -9,24 +9,24 @@ public class SavingsAccount {
     private BigDecimal balance;
     private String name;
 
-    public SavingsAccount(String balance, String name) {
-        this.balance = new BigDecimal(balance);
-        this.name = name;
-    }
-    public SavingsAccount() {
-        this.balance = BigDecimal.ZERO;
-    }
-    public SavingsAccount(String balance) {
-        this.balance = new BigDecimal(balance);
-    }
-    
+public SavingsAccount(String balance, String name) {
+    this.balance = new BigDecimal(balance);
+    this.name = name;
+}
+public SavingsAccount() {
+    this.balance = BigDecimal.ZERO;
+}
+public SavingsAccount(String balance) {
+    this.balance = new BigDecimal(balance);
+}
+
     public void calculateMonthlyInterest() {
         // a BigDecimal object representing the interest rate
 
         BigDecimal rate = new BigDecimal(SavingsAccount.rate);
-
+        
         // the divide method can be tricky, we need a rounding behavior and a scale
-
+        
         // "In the case of divide, the exact quotient
         // could have an infinitely long decimal expansion; for example, 1 divided by 3.
         // If the quotient has a nonterminating decimal expansion and the operation is
@@ -34,25 +34,33 @@ public class SavingsAccount {
         // Otherwise, the exact result of the division is returned, as done for other
         // operations."
         //
-
+        
         BigDecimal monthlyInterest = balance.multiply(rate).divide(TWELVE, 2, RoundingMode.UP);
         balance = balance.add(monthlyInterest);
     }
-
-    public void changeRate(String rate) {
+    
+    public static void changeRate(String rate) {
         SavingsAccount.rate = rate; 
     }
-
+    
+    public String getRate() {
+        return SavingsAccount.rate;
+    }
+    
     @Override
     public String toString() {
-        return balance.toString();
+        return String.format("%s ränta:%s saldo:%s", this.name, this.getRate(), this.getBalance());
     }
-   
+    
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getBalance() {
+        return balance.toString();
     }
 
     
