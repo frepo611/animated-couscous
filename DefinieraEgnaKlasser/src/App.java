da1214f2a6a94538d223c0bb0ce01b859a597641
Fredrik von Post <fredrik.vonpost@maxar.com>
2022-09-15 10:12:47 +0200

    add some explanatory printouts to App.java

--- DefinieraEgnaKlasser/src/App.java
+++ DefinieraEgnaKlasser/src/App.java
@@ -5,13 +5,15 @@ public class App {
         Rectangle rectangle2 = new Rectangle(20,20);
         Rectangle rectangle3 = new Rectangle(3.3, 4.5);
 
+        System.out.println("Skapar ett object med default ränta (=0)");
         SavingsAccount saving1 = new SavingsAccount("2000", "konto1");
         System.out.println(saving1);
+        System.out.println("Ändrar räntan till 1% och skapar ett nytt object");
         SavingsAccount.changeRate("0.01");
         SavingsAccount saving2 = new SavingsAccount("3000", "konto2");
         System.out.println(saving2);
 
-
+        System.out.println("Räntan har gemensam för alla object");
 
         for (int i = 0; i < 12; i++) {
             System.out.printf("Månad %1$d %2$s:%3$s %4$s:%5$s%n", i, saving1.getName(), saving1, saving2.getName(), saving2);
